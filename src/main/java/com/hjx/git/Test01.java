c23c5d58f1e3771ce7a5fb137c4abf7939612bea
hjx <hjx@qq.com>
2022-08-29 14:25:35 +0800

    hotfix commit

--- src/main/java/com/hjx/git/Test01.java
+++ src/main/java/com/hjx/git/Test01.java
@@ -7,5 +7,6 @@ package com.hjx.git;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello hot-fix");
     }
 }
