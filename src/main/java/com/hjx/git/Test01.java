0ed04bfa021e02a8d8f6da6864ef6438938b3b1b
hjx <hjx@qq.com>
2022-08-29 14:34:55 +0800

    hot-fix conflict commit

--- src/main/java/com/hjx/git/Test01.java
+++ src/main/java/com/hjx/git/Test01.java
@@ -8,5 +8,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello hot-fix");
+        System.out.println("hot-fix conflict");
     }
 }
